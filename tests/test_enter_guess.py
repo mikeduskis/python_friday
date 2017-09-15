f6284160387875c6473ceeb2bf38075d507c53f4
Mike Duskis <mike.duskis@protectwise.com>
2017-09-15 08:06:13 -0600

    Built the first unit test

--- tests/test_enter_guess.py
+++ tests/test_enter_guess.py
@@ -1,11 +1,23 @@
-from unittest import TestCase, main, skip
+from unittest import TestCase, main
+
+from expects import expect, match
+
+from codebreaker import Codebreaker
+from unit_test_tools import FakeConsole
 
 
 class TestEnterGuess(TestCase):
 
-    @skip("")
-    def test_nothing(self):
-        pass
+    def setUp(self):
+        self.console = FakeConsole()
+        self.sut = Codebreaker(console=self.console)
+        self.sut.start()
+
+    def test_returns_4_valid_chars_on_enter_guess(self):
+        # Enter a guess
+        self.console.stdin.write('1234\n')
+        expect(self.console.stdout.getvalue()).to(
+            match('[0\+\-]{4}'))
 
 
 if '__main__' == __name__:
