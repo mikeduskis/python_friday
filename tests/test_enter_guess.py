3b0db953386822f2f4394a9c6a24b9b8278f2c0c
Mike Duskis <mike.duskis@protectwise.com>
2017-09-15 06:42:03 -0600

    PEP 8

--- tests/test_enter_guess.py
+++ tests/test_enter_guess.py
@@ -1,10 +1,12 @@
 from unittest import TestCase, main, skip
 
+
 class TestEnterGuess(TestCase):
 
     @skip("")
     def test_nothing(self):
         pass
+
 
 if '__main__' == __name__:
     main()
