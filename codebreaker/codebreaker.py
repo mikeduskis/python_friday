a2db8c4f32b3b7290294651300e6fd1d396774b8
Mike Duskis <mike.duskis@protectwise.com>
2017-09-15 07:16:11 -0600

    PEP 8

--- codebreaker/codebreaker.py
+++ codebreaker/codebreaker.py
@@ -1,7 +1,7 @@
 class Codebreaker:
-	
-	def __init__(self, *, console):
-		self._console = console
 
-	def start(self):
-		self._console.stdout.write('Welcome to Code Breaker')
+    def __init__(self, *, console):
+        self._console = console
+
+    def start(self):
+        self._console.stdout.write('Welcome to Code Breaker')
